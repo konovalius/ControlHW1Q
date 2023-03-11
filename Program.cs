52f5cf4bcf46da1f0a4cd9870a1d0b1afd78cb19
Nikita Konovalov <veryeasy8@gmail.com>
2023-03-11 21:26:58 +0300

    create function filtering

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿string [] Array = new {"west","go","gz5","do",":-)"};
-string [] FilterArray = new string [];
+﻿string[] Array = new { "west", "go", "gz5", "do", ":-)" };
+string[] FilterArray = new string[];
+
+
+void FilteringArray(string[] FirstArray, string[] SecondArray)
+{
+    int k = 0;
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+        if (FirstArray[i].Length <= 3)
+        {
+            SecondArray[k]=FirstArray[i];
+            k++;
+        }
+    }
+}
